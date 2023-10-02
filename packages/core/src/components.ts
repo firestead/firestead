85dc9238941c219a7226c2e3b4bd74a3bc9735ef
DevJoghurt <johannes@disedia.com>
2023-10-02 17:58:41 +0200

    added default firebase logo

--- packages/core/src/components.ts
+++ packages/core/src/components.ts
@@ -1,6 +1,6 @@
 type Options = {
     cwd?: string
-    prefix?: string
+    prefix?: string | boolean
     global?: boolean
 }
 
@@ -8,14 +8,20 @@ const components = [
 {
     name: 'LayoutDefault',
     path: 'components/layouts/default.vue'
+},{
+    name: 'Logo',
+    prefix: false,
+    path: 'components/logo.vue'
 }]
 export function getComponents(options: Options = {
     cwd: process.cwd(),
     prefix: 'Fs'
 }){
     return components.map((component) => {
+        const prefix = component.prefix === false ? false : component.prefix || options.prefix
+        const name = prefix ? `${prefix}/${component.name}` : component.name
         return {
-            name: `${options.prefix}/${component.name}`,
+            name: name,
             filePath: `${options.cwd}/${component.path}`,
             watch: false,
             global: true
--- packages/core/src/runtime/components/layouts/default.vue
+++ packages/core/src/runtime/components/layouts/default.vue
@@ -4,8 +4,8 @@
       <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <!-- Sidebar component, swap this element with another sidebar if you like -->
         <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-          <div class="flex h-16 shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+          <div class="flex h-16 p-4 shrink-0 items-center">
+            <Logo class="h-12 w-auto" alt="Firestead"></Logo>
           </div>
           <nav class="flex flex-1 flex-col">
           </nav>
